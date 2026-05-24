1a314e2ad7615016296b30eb91bc2d6c2b1c6fa5
mosesbankole183-eng <mosesbankole183@gmail.com>
2026-05-24 14:45:30 +0100

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -2,21 +2,30 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
+import chatRoutes from "./routes/chatRoutes.js";
+
 dotenv.config();
 
 const app = express();
 
 app.use(cors());
+
 app.use(express.json());
 
+app.use("/chat", chatRoutes);
+
 app.get("/", (req, res) => {
+
   res.json({
     message: "MAICHAT backend working"
   });
+
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+
+  console.log(`Server running on ${PORT}`);
+
 });
