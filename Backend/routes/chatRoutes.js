694d48b904b50faa3baec8aa6adcb2432f06856d
mosesbankole183-eng <mosesbankole183@gmail.com>
2026-05-24 15:00:49 +0100

    Update chatRoutes.js

--- Backend/routes/chatRoutes.js
+++ Backend/routes/chatRoutes.js
@@ -1,14 +1,56 @@
 import express from "express";
 
+import OpenAI from "openai";
+
 const router = express.Router();
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY
+});
 
 router.post("/", async (req, res) => {
 
-  const { message } = req.body;
+  try{
 
-  res.json({
-    reply: `You said: ${message}`
-  });
+    const { message } = req.body;
+
+    const completion =
+      await openai.chat.completions.create({
+
+        model:"gpt-4.1-mini",
+
+        messages:[
+          {
+            role:"system",
+            content:
+              "You are MAICHAT, a smart futuristic AI assistant."
+          },
+
+          {
+            role:"user",
+            content:message
+          }
+        ]
+
+      });
+
+    const reply =
+      completion.choices[0]
+      .message.content;
+
+    res.json({
+      reply
+    });
+
+  }catch(error){
+
+    console.log(error);
+
+    res.status(500).json({
+      reply:"AI server error."
+    });
+
+  }
 
 });
 
